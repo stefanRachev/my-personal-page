ba3d7cb6980fc2189e805362870c54a03d6f770f
stefanRachev <starfystef@gmail.com>
2024-05-21 11:59:43 +0300

    form validation

--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -9,6 +9,7 @@ const formInitialState = {
 
 function Register() {
   const [formValues, setFormValues] = useState(formInitialState);
+  const [errors, setErrors] = useState({});
 
  
 
@@ -17,14 +18,36 @@ function Register() {
   
   };
 
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formValues.email) {
+      newErrors.email = "Email is required";
+    }
+    if (!formValues.password) {
+      newErrors.password = "Password is required";
+    }
+    if (formValues.password !== formValues.repeatPassword) {
+      newErrors.repeatPassword = "Passwords do not match";
+    }
+    if (!formValues.nickName) {
+      newErrors.nickName = "Name is required";
+    }
+    return newErrors;
+  };
+
   const resetFormHandler = () => {
     setFormValues(formInitialState)
   };
 
   const logHandler = (e) => {
     e.preventDefault();
-    console.log(formValues);
-    resetFormHandler()
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      console.log(formValues);
+      resetFormHandler();
+    }
   };
 
   return (
@@ -41,6 +64,7 @@ function Register() {
             value={formValues.email}
             onChange={formChangeHandler}
           />
+          {errors.email && <span className="error">{errors.email}</span>}
         </div>
         <div>
           <label htmlFor="password">Password</label>
@@ -51,6 +75,7 @@ function Register() {
             value={formValues.password}
             onChange={formChangeHandler}
           />
+          {errors.email && <span className="error">{errors.password}</span>}
         </div>
         <div>
           <label htmlFor="repeatPassword">Repeat Password</label>
@@ -61,6 +86,7 @@ function Register() {
             value={formValues.repeatPassword}
             onChange={formChangeHandler}
           />
+          {errors.email && <span className="error">{errors.repeatPassword}</span>}
         </div>
         <div>
           <label htmlFor="nickName">Name</label>
@@ -71,6 +97,7 @@ function Register() {
             value={formValues.nickName}
             onChange={formChangeHandler}
           />
+          {errors.email && <span className="error">{errors.nickName}</span>}
         </div>
         <div>
           <button type="submit">Register</button>
