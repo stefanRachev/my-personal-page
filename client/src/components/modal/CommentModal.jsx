04c99258a9b7640163f16b236c25e6979bce1056
stefanRachev <starfystef@gmail.com>
2024-06-12 17:08:29 +0300

    import props-type

--- client/src/components/modal/CommentModal.jsx
+++ client/src/components/modal/CommentModal.jsx
@@ -1,40 +1,53 @@
 import { useState } from "react";
 import styles from "./CommentModal.module.css";
+import PropTypes from "prop-types";
 
 const CommentModal = ({ image, closeModal }) => {
-    const [comment, setComment] = useState("");
-  
-    const handleCommentChange = (e) => {
-      setComment(e.target.value);
-    };
-  
-    const handleAddComment = () => {
-      // Логика за добавяне на коментар
-      console.log("Comment added:", comment);
-      setComment("");
-      closeModal();
-    };
-  
-    return (
-      <div className={styles.modalBackdrop} onClick={closeModal}>
-        <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-          <img src={image.imageUrl} alt={image.description} className={styles.image} />
-          <p className={styles.imageDescription}>{image.description}</p>
-          <div className={styles.commentSection}>
-            <textarea
-              className={styles.commentInput}
-              placeholder="Enter your comment..."
-              value={comment}
-              onChange={handleCommentChange}
-              rows={4}
-            />
-            <button className={styles.commentButton} onClick={handleAddComment}>
-              Add Comment
-            </button>
-          </div>
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleAddComment = () => {
+    // Логика за добавяне на коментар
+    console.log("Comment added:", comment);
+    setComment("");
+    closeModal();
+  };
+
+  return (
+    <div className={styles.modalBackdrop} onClick={closeModal}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        <img
+          src={image.imageUrl}
+          alt={image.description}
+          className={styles.image}
+        />
+        <p className={styles.imageDescription}>{image.description}</p>
+        <div className={styles.commentSection}>
+          <textarea
+            className={styles.commentInput}
+            placeholder="Enter your comment..."
+            value={comment}
+            onChange={handleCommentChange}
+            rows={4}
+          />
+          <button className={styles.commentButton} onClick={handleAddComment}>
+            Add Comment
+          </button>
         </div>
       </div>
-    );
-  };
-  
-  export default CommentModal;
+    </div>
+  );
+};
+
+CommentModal.propTypes = {
+  image: PropTypes.shape({
+    imageUrl: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
+
+export default CommentModal;
