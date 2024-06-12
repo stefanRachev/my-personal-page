1a0472f540baae07709f995e0193dc869fb4d482
stefanRachev <starfystef@gmail.com>
2024-06-12 17:36:56 +0300

    create comment model

---
+++ server/src/models/Comment.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
