5a12520ac5127518184c6f904f32e2edfcaf714d
stefanRachev <starfystef@gmail.com>
2024-06-16 17:53:06 +0300

    change style petGallery

--- client/src/components/personal/PetGallery.jsx
+++ client/src/components/personal/PetGallery.jsx
@@ -1,114 +1,76 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./PetGallery.module.css";
-import CommentModal from "../modal/CommentModal";
-import { fetchComments, addComment } from "./services/apiService";
 
-function PetGallery() {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [comment, setComment] = useState("");
-  const [comments, setComments] = useState([]);
-  const [error, setError] = useState(null);
+const petImages = [
+  {
+    id: 1,
+    imageUrl: "https://i.imgur.com/ruLoxNV.jpg",
+    description: "Sarah enjoys the day at our home.",
+  },
+  {
+    id: 2,
+    imageUrl: "https://i.imgur.com/Or5SwCq.jpg",
+    description: "Sarah taking a nap.",
+  },
+  {
+    id: 3,
+    imageUrl: "https://i.imgur.com/bIi0ShD.jpg",
+    description: "Sarah playing with my sister.",
+  },
+  {
+    id: 4,
+    imageUrl: "https://i.imgur.com/WGeSBXl.jpg",
+    description: "Sarah is curious.",
+  },
+  {
+    id: 5,
+    imageUrl: "https://i.imgur.com/9RYlr0D.jpg",
+    description: "Sarah is walking.",
+  },
+];
 
-  const petImages = [
-    {
-      id: 1,
-      imageUrl: "https://i.imgur.com/ruLoxNV.jpg",
-      description: "Sarah enjoys the day at our home.",
-    },
-    {
-      id: 2,
-      imageUrl: "https://i.imgur.com/Or5SwCq.jpg",
-      description: "Sarah taking a nap.",
-    },
-    {
-      id: 3,
-      imageUrl: "https://i.imgur.com/bIi0ShD.jpg",
-      description: "Sarah playing with my sister.",
-    },
-    {
-      id: 4,
-      imageUrl: "https://i.imgur.com/WGeSBXl.jpg",
-      description: "Sarah is curious.",
-    },
-    {
-      id: 5,
-      imageUrl: "https://i.imgur.com/9RYlr0D.jpg",
-      description: "Sarah is walking.",
-    },
-  ];
+const PetGallery = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
-    if (selectedImage) {
-      loadComments(selectedImage.id);
-    }
-  }, [selectedImage]);
-
-  const loadComments = async (imageId) => {
-    try {
-      const data = await fetchComments(imageId);
-      setComments(data);
-    } catch (err) {
-      setError(err.message);
-    }
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? petImages.length - 1 : prevIndex - 1
+    );
   };
 
-  const openModal = (image) => {
-    setSelectedImage(image);
-  };
-
-  const closeModal = () => {
-    setSelectedImage(null);
-  };
-
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
-
-  const handleAddComment = async () => {
-    try {
-      const newComment = { imageId: selectedImage.id, text: comment };
-      const addedComment = await addComment(newComment);
-      setComments([...comments, addedComment]);
-      setComment("");
-    } catch (err) {
-      setError(err.message);
-    }
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === petImages.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   return (
     <div className={styles.petGallery}>
       <h2>Pet Gallery</h2>
 
-      <div className={styles.gallery}>
-        {petImages.map((image) => (
-          <div
-            key={image.id}
-            className={styles.imageContainer}
-            onClick={() => openModal(image)}
-          >
-            <img
-              src={image.imageUrl}
-              alt={image.description}
-              className={styles.image}
-            />
-            <p className={styles.imageDescription}>{image.description}</p>
-          </div>
-        ))}
+      <div className={styles.imageContainer}>
+        <img
+          src={petImages[currentImageIndex].imageUrl}
+          alt={petImages[currentImageIndex].description}
+          className={styles.image}
+        />
+        <p className={styles.imageDescription}>
+          {petImages[currentImageIndex].description}
+        </p>
       </div>
 
-      {selectedImage && (
-        <CommentModal
-        image={selectedImage}
-        comments={comments}
-        closeModal={closeModal}
-        handleCommentChange={handleCommentChange}
-        handleAddComment={handleAddComment}
-        comment={comment}
-        error={error}
-      />
-      )}
+      <div className={styles.controls}>
+        <button className={styles.controlButton} onClick={prevImage}>
+          Previous
+        </button>
+        <button className={styles.controlButton} onClick={nextImage}>
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default PetGallery;
+
+
