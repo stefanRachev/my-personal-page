d098e341c8483c13f3c17015a5b73d9a17e1841d
stefanRachev <starfystef@gmail.com>
2024-08-10 16:01:13 +0300

    create css ,EmailForm

--- client/src/components/personal/EmailForm.jsx
+++ client/src/components/personal/EmailForm.jsx
@@ -1,27 +1,25 @@
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
+import styles from "./EmailForm.module.css";
 
 const host = "http://localhost:3001";
 
 const EmailForm = () => {
-  
   const { user, loading } = useAuth();
- 
+
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
 
-  
   if (loading) {
     return <p>Loading...</p>;
   }
 
-  
   if (!user) {
     return <Navigate to="/users/login" />;
   }
 
-
+  console.log(user);
 
   // Функция за обработка на изпращането на формата
   const handleSubmit = async (e) => {
@@ -33,7 +31,7 @@ const EmailForm = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        name: user.name, // Използване на името на потребителя от контекста
+        name: user.nickName, // Използване на името на потребителя от контекста
         email: user.email, // Използване на имейла на потребителя от контекста
         subject,
         message,
@@ -52,31 +50,45 @@ const EmailForm = () => {
 
   // Връщане на JSX компонента за формата
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <div>
-        <label>Name:</label>
-        <input type="text" value={user.name} disabled />
+        <label className={styles.label}>Name:</label>
+        <input
+          type="text"
+          value={user.nickName}
+          disabled
+          className={`${styles.input} ${styles.disabledInput}`}
+        />
       </div>
       <div>
-        <label>Email:</label>
-        <input type="email" value={user.email} disabled />
+        <label className={styles.label}>Email:</label>
+        <input
+          type="email"
+          value={user.email}
+          disabled
+          className={`${styles.input} ${styles.disabledInput}`}
+        />
       </div>
       <div>
-        <label>Subject:</label>
+        <label className={styles.label}>Subject:</label>
         <input
           type="text"
           value={subject}
           onChange={(e) => setSubject(e.target.value)}
+          className={styles.input}
         />
       </div>
       <div>
-        <label>Message:</label>
+        <label className={styles.label}>Message:</label>
         <textarea
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          className={styles.textarea}
         />
       </div>
-      <button type="submit">Send Email</button>
+      <button type="submit" className={styles.button}>
+        Send Email
+      </button>
     </form>
   );
 };
