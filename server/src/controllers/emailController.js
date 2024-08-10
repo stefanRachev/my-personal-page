b1bac19fe00e30bb74704bf6a0e56ecf93dbfa09
stefanRachev <starfystef@gmail.com>
2024-08-10 16:42:15 +0300

    install nodemailer,create controller emailController and rotes

---
+++ server/src/controllers/emailController.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail', // Или друг доставчик на email услуга
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: subject,
+    text: `Име: ${name}\nИмейл: ${email}\nСъобщение: ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ message: 'Failed to send email', error });
+  }
+};
+
+module.exports = {
+  sendEmail,
+};
+
