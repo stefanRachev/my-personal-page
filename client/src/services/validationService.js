451cb65c52618b11be726cbb5e1f8d9a5e6a638a
stefanRachev <starfystef@gmail.com>
2024-08-15 13:57:47 +0300

    firebase init,correct node version -18

--- client/src/services/validationService.js
+++ client/src/services/validationService.js
@@ -1,10 +1,8 @@
-// Валидация на имейл
 export const validateEmail = (email) => {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(String(email).toLowerCase());
 };
 
-// Валидация на паролата
 const validatePassword = (password) => {
   const minLength = 6;
   const hasUpperCase = /[A-Z]/.test(password);
@@ -22,18 +20,15 @@ const validatePassword = (password) => {
   return null;
 };
 
-// Валидация на формата за регистрация
 export const validateRegistrationForm = (formValues) => {
   const errors = {};
 
-  // Валидация на имейл
   if (!formValues.email) {
     errors.email = "Email is required";
   } else if (!validateEmail(formValues.email)) {
     errors.email = "Invalid email address";
   }
 
-  // Валидация на паролата
   if (!formValues.password) {
     errors.password = "Password is required";
   } else {
@@ -43,12 +38,10 @@ export const validateRegistrationForm = (formValues) => {
     }
   }
 
-  // Проверка за съвпадение на паролите
   if (formValues.password !== formValues.repeatPassword) {
     errors.repeatPassword = "Passwords do not match";
   }
 
-  // Валидация на име
   if (!formValues.nickName) {
     errors.nickName = "Name is required";
   }
